281e0c84c597a27fa7121f3fa87ec2dda0b3208a
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-10-22 13:58:15 +1300

    Add IonDateTime to format timer (#9)

--- TrackFit/src/components/Stopwatch.tsx
+++ TrackFit/src/components/Stopwatch.tsx
@@ -3,13 +3,50 @@
  *
  * References:
  * https://forum.ionicframework.com/t/making-buttons-side-by-side/162278/7
+ * https://www.digitalocean.com/community/tutorials/react-countdown-timer-react-hooks
  */
-import React, { useState } from "react";
-import { IonButton, IonGrid, IonLabel, IonRow, IonCol } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import {
+  IonButton,
+  IonGrid,
+  IonLabel,
+  IonRow,
+  IonCol,
+  IonText,
+  IonDatetime,
+} from "@ionic/react";
 
 const Stopwatch: React.FC = () => {
+  let start: number = 0;
+  let hours: number = 12;
+  let minutes: number = 15;
+  let seconds: number = 10;
+  /**
+   * Start the Stopwatch.
+   */
+  const startStopwatch = () => {};
+
+  /**
+   * Stop the Stopwatch.
+   */
+  const stopStopwatch = () => {};
+
+  /**
+   * Reset the Stopwatch to zero.
+   */
+  const resetStopwatch = () => {};
+
   return (
     <IonGrid>
+      <IonRow>
+        <IonCol>
+          {/* <IonText>{hours + ":" + minutes + ":" + seconds}</IonText> */}
+          <IonDatetime
+            value={hours + ":" + minutes + ":" + seconds}
+            displayFormat="HH:mm:ss"
+          ></IonDatetime>
+        </IonCol>
+      </IonRow>
       <IonRow>
         <IonCol>
           <IonButton
