5744cd5b06dd761278c1aa5a558fc4464acef094
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-10-22 13:22:50 +1300

    Add Start Stop buttons for Stopwatch component #9

--- TrackFit/src/components/Stopwatch.tsx
+++ TrackFit/src/components/Stopwatch.tsx
@@ -1,10 +1,39 @@
 /**
  * The Stopwatch component keeps track of the time the user has spent working out.
+ *
+ * References:
+ * https://forum.ionicframework.com/t/making-buttons-side-by-side/162278/7
  */
-import React, { useState } from 'react';
-import { IonButton, IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from "react";
+import { IonButton, IonGrid, IonLabel, IonRow, IonCol } from "@ionic/react";
 
-const Stopwatch = () => {
+const Stopwatch: React.FC = () => {
+  return (
+    <IonGrid>
+      <IonRow>
+        <IonCol>
+          <IonButton
+            expand="full"
+            onClick={() => {
+              console.log("start");
+            }}
+          >
+            <IonLabel>START</IonLabel>
+          </IonButton>
+        </IonCol>
+        <IonCol>
+          <IonButton
+            expand="full"
+            onClick={() => {
+              console.log("stop");
+            }}
+          >
+            <IonLabel>STOP</IonLabel>
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  );
+};
 
-
-}
+export default Stopwatch;
