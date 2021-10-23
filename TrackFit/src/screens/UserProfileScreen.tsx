60058ac0518b897fc310e91a600d7336f4058ff0
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-10-23 13:17:14 +1300

    Add text fields to User Profile screen (#7)

--- TrackFit/src/screens/UserProfileScreen.tsx
+++ TrackFit/src/screens/UserProfileScreen.tsx
@@ -1,5 +1,12 @@
+/**
+ * The User Profile screen requests the user to input basic information (name, age, weight) about themselves.
+ * The user is also prompted to set their goals that will be used throught out the app.
+ *
+ * References:
+ * https://ionicframework.com/docs/api/input
+ * https://www.tutorialandexample.com/ionic-input/
+ */
 import {
-  IonBackButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -8,14 +15,22 @@ import {
   IonToolbar,
   IonButton,
   IonLabel,
+  IonInput,
+  IonList,
+  IonItem,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import { useIonRouter } from "@ionic/react";
 import { dynamicNavigate } from "../functions/navigation";
+import { useState } from "react";
 
 const UserProfileScreen: React.FC = () => {
   //	Initializing our router
   const router = useIonRouter();
+
+  const [name, setName] = useState<string>();
+  const [age, setAge] = useState<number>();
+  const [weight, setWeight] = useState<number>();
 
   return (
     <IonPage>
@@ -27,18 +42,40 @@ const UserProfileScreen: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">test</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="User Profile" />
+        <IonList>
+          <IonItem>
+            <IonLabel>Enter Name: </IonLabel>
+            <IonInput
+              value={name}
+              placeholder="eg. Spongebob"
+              onIonChange={(e) => setName(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Enter Age: </IonLabel>
+            <IonInput
+              type="number"
+              value={age}
+              placeholder="eg. 25"
+              onIonChange={(e) => setAge(parseInt(e.detail.value!, 10))}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Enter Weight (kg): </IonLabel>
+            <IonInput
+              type="number"
+              value={weight}
+              placeholder="eg. 50"
+              onIonChange={(e) => setWeight(parseInt(e.detail.value!, 10))}
+            ></IonInput>
+          </IonItem>
+        </IonList>
         <IonButton
           onClick={() => {
             dynamicNavigate("/SetGoalScreen", "forward", router);
           }}
         >
-          <IonLabel>Set Goal</IonLabel>
+          <IonLabel>Set Goals</IonLabel>
         </IonButton>
       </IonContent>
     </IonPage>
